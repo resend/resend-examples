065c33d85ada8e10f9390bb83921210188f59dae
Zeno Rocha <zno.rocha@gmail.com>
2023-06-01 22:46:16 -0700

    feat: Add dynamic config

--- with-vercel-edge-functions/app/api/send/route.ts
+++ with-vercel-edge-functions/app/api/send/route.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from 'next/server';
 
 export const runtime = 'edge';
+export const dynamic = 'force-dynamic';
 
 const RESEND_API_KEY = 're_123456789';
 
-export async function GET() {
+export async function POST() {
     const res = await fetch('https://api.resend.com/emails', {
         method: 'POST',
         headers: {
