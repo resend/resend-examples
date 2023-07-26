8296fbb035a3adbdfa0469e17d860206754e036e
Bu Kinoshita <bukinoshita@gmail.com>
2023-07-25 23:31:50 -0300

    fix: Minor tweak

--- with-express-react-email/src/lib/resend.ts
+++ with-express-react-email/src/lib/resend.ts
@@ -1,5 +1,3 @@
 import { Resend } from 'resend';
 
-export const resend = new Resend(
-  're_FzrbPgFo_MoiqEytvDKhBDQDqLFdycwBA' || process.env.RESEND_API_KEY!,
-);
+export const resend = new Resend(process.env.RESEND_API_KEY!);
